fc2614577c21578b73dabf0490b8ca5532fb012e
Mari Selvam <151715381+MariSelvam04@users.noreply.github.com>
2024-04-21 19:44:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Registrationform from './Registrationform';
+function App() {
+
+
+  return (
+    <div className="App">
+      <Registrationform>
+
+      </Registrationform>
+    </div>
+  );
+}
+
+export default App;
